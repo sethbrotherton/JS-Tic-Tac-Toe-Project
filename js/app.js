b0bd72cb0943f0bc3922624888c9fbd41710c832
sethbrotherton <sethbrotherton@yahoo.com>
2018-08-21 03:54:52 -0400

    Added play against computer: easy

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ const box = $('.box');
 const boxHover = $('.box:hover');
 let oBoxes = [];
 let xBoxes = [];
+let playingEasyComputer = false;
+const $player1 = $('#player1');
+const $player2 = $('#player2');
+
 
 function addStartButtonEvent() {
   startButton.on('click', function () {
@@ -15,6 +19,72 @@ function addStartButtonEvent() {
   });
 }
 addStartButtonEvent();
+
+
+$player1.css('display', 'inline-block');
+const nameInput = document.createElement('input');
+const nameButton = document.createElement('button');
+nameButton.classList.add('button');
+nameButton.classList.add('input-button');
+nameButton.innerHTML = 'Submit';
+nameInput.setAttribute('placeHolder', 'Enter your name');
+nameInput.setAttribute('color', 'black');
+$player1.append(nameInput);
+$player1.append(nameButton);
+nameButton.addEventListener('click', () => {
+  playerName = nameInput.value;
+  $player1.append(`<h1 id="player-name">${playerName}</h1>`);
+  $('#player-name').css('display', 'block').css('text-align', 'center');
+  nameInput.remove();
+  nameButton.remove();
+});
+
+function addPlayComputerButton() {
+  const playComputerButton = document.createElement('button');
+  playComputerButton.innerHTML = 'Play Computer: Easy';
+  playComputerButton.classList.add('button');
+  playComputerButton.classList.add('difficulty-button');
+  playComputerButton.setAttribute('id', 'play-computer-easy');
+  board.append(playComputerButton);
+}
+addPlayComputerButton();
+
+function addPlayMediumComputerButton() {
+  const playMediumComputerButton = document.createElement('button');
+  playMediumComputerButton.innerHTML = 'Play Computer: Medium';
+  playMediumComputerButton.classList.add('button');
+  playMediumComputerButton.classList.add('difficulty-button');
+  playMediumComputerButton.setAttribute('id', 'play-computer-medium');
+  board.append(playMediumComputerButton);
+}
+addPlayMediumComputerButton();
+
+function addPlayHardComputerButton() {
+  const playHardComputerButton = document.createElement('button');
+  playHardComputerButton.innerHTML = 'Play Computer: Hard';
+  playHardComputerButton.classList.add('button');
+  playHardComputerButton.classList.add('difficulty-button');
+  playHardComputerButton.setAttribute('id', 'play-computer-hard');
+  board.append(playHardComputerButton);
+}
+addPlayHardComputerButton();
+
+const $playEasyComputerButton = $('#play-computer-easy');
+function turnEasyComputerPlayOn() {
+  $playEasyComputerButton.on('click', () => {
+    if (playingEasyComputer == false) {
+      playingEasyComputer = true;
+      $playEasyComputerButton.css('backgroundColor', 'red');
+      $playEasyComputerButton.innerHTML = 'Playing Computer: Easy';
+    } else {
+      playingEasyComputer = false;
+      $playEasyComputerButton.css('backgroundColor', 'white');
+    }
+  });
+}
+turnEasyComputerPlayOn();
+
+
 
 function addMouseoverSvg (svg) {
   for (let i = 0; i < box.length; i++) {
@@ -55,7 +125,29 @@ addMouseOut();
 function addClickSvg () {
   for (let i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function(e) {
-      if ($('#player1').hasClass('active')) {
+      if ($('#player1').hasClass('active') && playingEasyComputer == false) {
+        oBoxes.push(box[i]);
+        box[i].classList.add('box-filled-1');
+        $('#player1').removeClass('active');
+        $('#player2').addClass('active');
+        addMouseoverSvg("url('img/x.svg')");
+        box[i].style.pointerEvents = 'none';
+        if (nameInput.value != '') {
+          let winnerName = nameInput.value.toUpperCase();
+          displayWinner('box-filled-1', winnerName, 'screen-win-one');
+        } else {
+          displayWinner('box-filled-1', 'O', 'screen-win-one');
+        }
+      } else if ($('#player2').hasClass('active') && playingEasyComputer == false) {
+        xBoxes.push(box[i]);
+        box[i].classList.add('box-filled-2');
+        $('#player2').removeClass('active');
+        $('#player1').addClass('active');
+        addMouseoverSvg("url('img/o.svg')");
+        box[i].style.pointerEvents = 'none';
+        displayWinner('box-filled-2', 'X', 'screen-win-two');
+        findAvailableSquares();
+      } else if ($('#player1').hasClass('active') && playingEasyComputer == true) {
         oBoxes.push(box[i]);
         box[i].classList.add('box-filled-1');
         $('#player1').removeClass('active');
@@ -63,19 +155,17 @@ function addClickSvg () {
         addMouseoverSvg("url('img/x.svg')");
         box[i].style.pointerEvents = 'none';
         displayWinner('box-filled-1', 'O', 'screen-win-one');
-     } else if ($('#player2').hasClass('active')) {
-       xBoxes.push(box[i]);
-        box[i].classList.add('box-filled-2');
+        findAvailableSquares();
+        setTimeout(randomTurn, 1000);
         $('#player2').removeClass('active');
         $('#player1').addClass('active');
         addMouseoverSvg("url('img/o.svg')");
-        box[i].style.pointerEvents = 'none';
-        displayWinner('box-filled-2', 'X', 'screen-win-two');
       }
     });
   }
 }
 addClickSvg();
+
 
 const winningCombos = [
   [box[0], box[1], box[2]],
@@ -93,7 +183,7 @@ function displayWinner(boxClass, team, winnerClass) {
   for (let i = 0; i < winningCombos.length; i++) {
     const threeInARow = ((winningCombos[i][0].classList.contains(boxClass)) && (winningCombos[i][1].classList.contains(boxClass)) && (winningCombos[i][2].classList.contains(boxClass)));
     if (threeInARow === true) {
-      console.log(`The ${team}s Won!`);
+      console.log(`${nameInput.value} Won!`);
       board.hide();
       $('body').html(`<div class="screen screen-win" id="finish">
         <header>
@@ -103,7 +193,7 @@ function displayWinner(boxClass, team, winnerClass) {
         </header>
       </div>`);
       $('#finish').addClass(winnerClass);
-        $('.message').text('WINNER');
+        $('.message').text(`${team} IS A WINNER!!!`);
         addNewGameButton();
         return
     } else if ((oBoxes.length + xBoxes.length === 9) && (threeInARow === false)) {
@@ -135,10 +225,14 @@ function addNewGameButton() {
     svgBackgoundTurns();
     clearBoard();
     board.show();
+    turnEasyComputerPlayOn();
   });
 }
 
 function clearBoard() {
+  $('#player1').addClass('active');
+  $('#player2').removeClass('active');
+  addMouseoverSvg("url('img/o.svg')");
   for (let i = 0; i < box.length; i++) {
     box[i].classList.remove('box-filled-1');
     box[i].classList.remove('box-filled-2');
@@ -147,4 +241,21 @@ function clearBoard() {
     xBoxes = [];
     oBoxes = [];
   }
+}
+
+
+let availableSquares;
+function findAvailableSquares () {
+  availableSquares = [];
+  for (let i = 0; i < box.length; i++) {
+    if (!box[i].classList.contains('box-filled-1') && !box[i].classList.contains('box-filled-2')) {
+      availableSquares.push(box[i]);
+    }
+  }
+}
+let randomAvailableSquare;
+function randomTurn() {
+  let randomXSquare = availableSquares[Math.floor(Math.random() * availableSquares.length)].classList.add('box-filled-2');
+  xBoxes.push(randomXSquare);
+  displayWinner('box-filled-2', 'X', 'screen-win-two');
 }
