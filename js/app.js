494a78e90112a44e8047db55d4a0a223512ffc04
sethbrotherton <sethbrotherton@yahoo.com>
2018-08-28 00:08:34 -0400

    Hard difficulty level added

--- js/app.js
+++ js/app.js
@@ -51,15 +51,15 @@ function addPlayEasyComputerButton() {
 }
 addPlayEasyComputerButton();
 
-function addPlayMediumComputerButton() {
-  const playMediumComputerButton = document.createElement('button');
-  playMediumComputerButton.innerHTML = 'Play Computer: Medium';
-  playMediumComputerButton.classList.add('button');
-  playMediumComputerButton.classList.add('difficulty-button');
-  playMediumComputerButton.setAttribute('id', 'play-computer-medium');
-  board.append(playMediumComputerButton);
-}
-addPlayMediumComputerButton();
+// function addPlayMediumComputerButton() {
+//   const playMediumComputerButton = document.createElement('button');
+//   playMediumComputerButton.innerHTML = 'Play Computer: Medium';
+//   playMediumComputerButton.classList.add('button');
+//   playMediumComputerButton.classList.add('difficulty-button');
+//   playMediumComputerButton.setAttribute('id', 'play-computer-medium');
+//   board.append(playMediumComputerButton);
+// }
+// addPlayMediumComputerButton();
 
 function addPlayHardComputerButton() {
   const playHardComputerButton = document.createElement('button');
@@ -72,43 +72,43 @@ function addPlayHardComputerButton() {
 addPlayHardComputerButton();
 
 const $playEasyComputerButton = $('#play-computer-easy');
-const $playMediumComputerButton = $('#play-computer-medium');
+//const $playMediumComputerButton = $('#play-computer-medium');
 const $playHardComputerButton = $('#play-computer-hard');
 function turnComputerPlayOn() {
   $playEasyComputerButton.on('click', () => {
     if (playingEasyComputer == false) {
       playingEasyComputer = true;
-      playingMediumComputer = false;
+    //  playingMediumComputer = false;
       playingHardComputer = false;
       $playEasyComputerButton.css('backgroundColor', 'red');
-      $playMediumComputerButton.css('backgroundColor', 'white');
+    //  $playMediumComputerButton.css('backgroundColor', 'white');
       $playHardComputerButton.css('backgroundColor', 'white');
     } else {
       playingEasyComputer = false;
       $playEasyComputerButton.css('backgroundColor', 'white');
     }
   });
-    $playMediumComputerButton.on('click', () => {
-      if (playingMediumComputer == false) {
-        playingMediumComputer = true;
-        playingEasyComputer = false;
-        playingHardComputer = false;
-        $playMediumComputerButton.css('backgroundColor', 'red');
-        $playEasyComputerButton.css('backgroundColor', 'white');
-        $playHardComputerButton.css('backgroundColor', 'white');
-      } else {
-        playingMediumComputer = false;
-        $playMediumComputerButton.css('backgroundColor', 'white');
-      }
-  });
+  //   $playMediumComputerButton.on('click', () => {
+  //     if (playingMediumComputer == false) {
+  //       playingMediumComputer = true;
+  //       playingEasyComputer = false;
+  //       playingHardComputer = false;
+  //       $playMediumComputerButton.css('backgroundColor', 'red');
+  //       $playEasyComputerButton.css('backgroundColor', 'white');
+  //       $playHardComputerButton.css('backgroundColor', 'white');
+  //     } else {
+  //       playingMediumComputer = false;
+  //       $playMediumComputerButton.css('backgroundColor', 'white');
+  //     }
+  // });
     $playHardComputerButton.on('click', () => {
       if (playingHardComputer == false) {
         playingHardComputer = true;
         playingEasyComputer = false;
-        playingMediumComputer = false;
+    //    playingMediumComputer = false;
         $playHardComputerButton.css('backgroundColor', 'red');
         $playEasyComputerButton.css('backgroundColor', 'white');
-        $playMediumComputerButton.css('backgroundColor', 'white');
+    //    $playMediumComputerButton.css('backgroundColor', 'white');
       } else {
         playingHardComputer = false;
         $playHardComputerButton.css('backgroundColor', 'white');
@@ -155,134 +155,112 @@ function addMouseOut () {
 addMouseOut();
 
 
+function activatePlayerTwo() {
+  $('#player1').removeClass('active');
+  $('#player2').addClass('active');
+  addMouseoverSvg("url('img/x.svg')");
+}
+
+
+function activatePlayerOne() {
+  $('#player2').removeClass('active');
+  $('#player1').addClass('active');
+  addMouseoverSvg("url('img/o.svg')");
+}
+
 function addClickSvg () {
   for (let i = 0; i < box.length; i++) {
     box[i].addEventListener('click', function(e) {
       if ($('#player1').hasClass('active') && playingEasyComputer == false && playingHardComputer == false) {
         oBoxes.push(box[i]);
         box[i].classList.add('box-filled-1');
-        $('#player1').removeClass('active');
-        $('#player2').addClass('active');
-        addMouseoverSvg("url('img/x.svg')");
+        activatePlayerTwo();
         box[i].style.pointerEvents = 'none';
-        if (nameInput.value != '') {
-          let winnerName = nameInput.value.toUpperCase();
-          displayWinner('box-filled-1', winnerName, 'screen-win-one');
-        } else {
-          displayWinner('box-filled-1', 'O', 'screen-win-one');
-        }
+        showWinnerWithName();
       } else if ($('#player2').hasClass('active') && playingEasyComputer == false && playingHardComputer == false) {
         xBoxes.push(box[i]);
         box[i].classList.add('box-filled-2');
-        $('#player2').removeClass('active');
-        $('#player1').addClass('active');
-        addMouseoverSvg("url('img/o.svg')");
+        activatePlayerOne();
         box[i].style.pointerEvents = 'none';
         displayWinner('box-filled-2', 'X', 'screen-win-two');
         findAvailableSquares();
       } else if ($('#player1').hasClass('active') && playingEasyComputer == true) {
         oBoxes.push(box[i]);
         box[i].classList.add('box-filled-1');
-        $('#player1').removeClass('active');
-        $('#player2').addClass('active');
-        addMouseoverSvg("url('img/x.svg')");
+        activatePlayerTwo();
         box[i].style.pointerEvents = 'none';
         displayWinner('box-filled-1', 'O', 'screen-win-one');
         findAvailableSquares();
         setTimeout(randomTurn, 1000);
-        $('#player2').removeClass('active');
-        $('#player1').addClass('active');
-        addMouseoverSvg("url('img/o.svg')");
-        if (nameInput.value != '') {
-          let winnerName = nameInput.value.toUpperCase();
-          displayWinner('box-filled-1', winnerName, 'screen-win-one');
-        } else {
-          displayWinner('box-filled-1', 'O', 'screen-win-one');
-        }
+        activatePlayerOne();
+        showWinnerWithName();
       }  else if ($('#player1').hasClass('active') && playingEasyComputer == false && playingHardComputer == true) {
         oBoxes.push(box[i]);
         box[i].classList.add('box-filled-1');
-        $('#player1').removeClass('active');
-        $('#player2').addClass('active');
-        addMouseoverSvg("url('img/x.svg')");
+        activatePlayerTwo();
         box[i].style.pointerEvents = 'none';
-        displayWinner('box-filled-1', 'O', 'screen-win-one');
+        showWinnerWithName();
         computerPlayingHard();
-        $('#player2').removeClass('active');
-        $('#player1').addClass('active');
-        addMouseoverSvg("url('img/o.svg')");
+        displayWinner('box-filled-2', 'X', 'screen-win-two');
+        activatePlayerOne();
       }
     });
   }
 }
 addClickSvg();
 
+
+function showWinnerWithName() {
+  if (nameInput.value != '') {
+    let winnerName = nameInput.value.toUpperCase();
+    displayWinner('box-filled-1', winnerName, 'screen-win-one');
+  } else {
+    displayWinner('box-filled-1', 'O', 'screen-win-one');
+  }
+}
+
+
 let hardXSquares;
 function computerPlayingHard () {
   hardXSquares = [];
   for (let i = 0; i < winningCombos.length; i++) {
-    //hardXSquares = [];
-    if (winningCombos[i][0].classList.contains('box-filled-1') && winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-1')) {
-      //winningCombos[i][2].classList.add('box-filled-2');
-      console.log('1st');
-      hardXSquares = [];
-      hardXSquares.push(winningCombos[i][2]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-    //  return
-  } else if (winningCombos[i][1].classList.contains('box-filled-1') && winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][0].classList.contains('box-filled-1')) {
-      //winningCombos[i][1].classList.add('box-filled-2');
-      console.log('2');
-      hardXSquares = [];
-      hardXSquares.push(winningCombos[i][0]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-    //  return
-    } else if (winningCombos[i][0].classList.contains('box-filled-1') && winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-1')) {
-      console.log('3');
-      //winningCombos[i][1].classList.add('box-filled-2');
-      hardXSquares = [];
-      hardXSquares.push(winningCombos[i][1]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-    //  return
-    }
-
-    else if (winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-1')) {
-      //winningCombos[i][2].classList.add('box-filled-2');
+    if (winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][0].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-2') && !winningCombos[i][0].classList.contains('box-filled-2')) {
       console.log('4');
-      hardXSquares = [];
       hardXSquares.push(winningCombos[i][2]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-    //  return
-    } else if (winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-1')) {
-      //winningCombos[i][1].classList.add('box-filled-2');
+      xBoxes.push(winningCombos[i][2]);
+  } else if (winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][0].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-2') && !winningCombos[i][0].classList.contains('box-filled-2')) {
       console.log('5');
-      hardXSquares = [];
       hardXSquares.push(winningCombos[i][1]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-    //  return
-    }  else if (winningCombos[i][0].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-1')) {
+      xBoxes.push(winningCombos[i][1]);
+    }  else if (winningCombos[i][0].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-2') && !winningCombos[i][2].classList.contains('box-filled-2')) {
       console.log('6');
-      hardXSquares = [];
       hardXSquares.push(winningCombos[i][1]);
-      // let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
-      // xBoxes.push(randomHardXSquare);
-      // displayWinner('box-filled-2', 'X', 'screen-win-two');
-      //return
+      xBoxes.push(winningCombos[i][1]);
   }
-
-}
+    else if (winningCombos[i][0].classList.contains('box-filled-1') && winningCombos[i][1].classList.contains('box-filled-1') && (!winningCombos[i][2].classList.contains('box-filled-1') && !winningCombos[i][2].classList.contains('box-filled-2'))) {
+      console.log('1st');
+      hardXSquares.push(winningCombos[i][2]);
+      winningCombos[i][2].classList.add('box-filled-2');
+      xBoxes.push(winningCombos[i][2]);
+      return
+  } else if (winningCombos[i][1].classList.contains('box-filled-1') && winningCombos[i][2].classList.contains('box-filled-1') && (!winningCombos[i][0].classList.contains('box-filled-1') && !winningCombos[i][0].classList.contains('box-filled-2'))) {
+      console.log('2');
+      hardXSquares.push(winningCombos[i][0]);
+      winningCombos[i][0].classList.add('box-filled-2');
+      xBoxes.push(winningCombos[i][0]);
+      return
+    } else if (winningCombos[i][0].classList.contains('box-filled-1') && winningCombos[i][2].classList.contains('box-filled-1') && (!winningCombos[i][1].classList.contains('box-filled-1') && !winningCombos[i][1].classList.contains('box-filled-2'))) {
+      console.log('3');
+      hardXSquares.push(winningCombos[i][1]);
+      winningCombos[i][1].classList.add('box-filled-2');
+      xBoxes.push(winningCombos[i][1]);
+      return
+    }
+  }
 let randomHardXSquare = hardXSquares[Math.floor(Math.random() * hardXSquares.length)].classList.add('box-filled-2');
 xBoxes.push(randomHardXSquare);
-//displayWinner('box-filled-2', 'X', 'screen-win-two');
+displayWinner('box-filled-2', 'X', 'screen-win-two');
+return
 }
 
 
